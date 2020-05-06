57ac100a09aeeeff956e9e69ea1c3d8b419c13d8
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-05-06 16:08:15 +0300

    only test

---
+++ Lab_13_1/Lab_13_1/Valid_Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_13_1
+{
+    class Valid_Person
+    {
+        public PersonListProcessor[] Lista { get; }
+
+        public Valid_Person(PersonListProcessor[] lista)
+        {
+            this.Lista = lista ?? new PersonListProcessor[0];
+        }
+
+
+    }
+}
